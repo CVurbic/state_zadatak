160484481b0b53913bd015ed5de3c60788d465a2
hyperv <hyperv@ucione.local>
2024-02-27 20:43:10 +0100

    zamijenjeno sa state componentom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 
 import './App.css';
 import GodineButton from './components/GodineButton';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import IpakNemoj from './components/IpakNemoj';
 import mladaSlika from "./imgs/mladi.jpg"
 import stariSlika from "./imgs/OIP.jpg"
 
 
-function App() {
+/* function App() {
   const [đuro, setĐuro] = useState({ime: "Đuro", godine: "5 godina", imgLink: mladaSlika})
 
   function handleĐuroGodine(đurica){
@@ -41,6 +41,53 @@ function App() {
       </header>
     </div>
   );
+} */
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state ={
+      đuro: {ime: "Đuro", godine: "5 godina", imgLink: mladaSlika}
+    };
+    this.handleĐuroGodine = this.handleĐuroGodine.bind(this);
+    this.handleVratiGodine = this.handleVratiGodine.bind(this);
+  }
+
+  handleĐuroGodine(đurica){
+    console.log(this.state.đuro)
+    const newĐuro = {...this.đuro}
+    newĐuro.godine = `${đurica} godina`
+    newĐuro.imgLink = stariSlika;
+    this.setState({đuro: newĐuro})
+  }
+  handleVratiGodine(){
+    this.setState({đuro: {ime: "Đuro", godine: "5 godina", imgLink: mladaSlika}})
+  }
+
+  render(){
+    const đuro = this.state.đuro
+    console.log(đuro)
+    return(
+        <div className="App">
+          <header className="App-header">
+            
+             <img src={đuro.imgLink} style={{maxHeight:"500px"}} alt="logo" />
+     
+            
+            <GodineButton
+              handleĐuroGodine= {this.handleĐuroGodine}
+            />
+            <p>
+              {đuro.ime}, {đuro.godine}
+            </p>
+            <IpakNemoj
+             handleVratiGodine= {this.handleVratiGodine}
+            />
+            
+          </header>
+        </div>
+      
+    )
+  }
 }
 
 export default App;
